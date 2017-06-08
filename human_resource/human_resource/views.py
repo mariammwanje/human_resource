a009aa7dc147decf8f087e8629826a016bdd0b3a
mariammwanje <mariamnanono498gmail.com>
2017-06-08 17:26:05 +0300

    Template Additition and intergration

---
+++ human_resource/human_resource/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# from django.contrib.auth.decorators import login_required
+# @login_required(login_url='accounts:login')
+
+
+def home(request):
+    return render(request, '../templates/landing.html')
